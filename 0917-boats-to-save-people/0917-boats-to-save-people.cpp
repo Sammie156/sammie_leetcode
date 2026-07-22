4979342ec366bfd5337e71f2eea82247e14520f5
Samanwaya Datta <64217426+Sammie156@users.noreply.github.com>
2026-07-22 11:48:28 +0530

    Time: 15 ms (73.38%), Space: 45.8 MB (39.6%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+
+        int count = 0;
+        int left = 0, right = people.size() - 1;
+        while (left < right) {
+            if (people[left] + people[right] > limit) {
+                count++;
+                right--;
+            } else {
+                left++;
+                right--;
+                count++;
+            }
+        }
+
+        if (left == right) count++;
+
+        return count;
+    }
+};
