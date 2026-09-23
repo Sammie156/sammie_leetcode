96799c1c79b3150683d597fdb51e2385ae7ad642
Samanwaya Datta <64217426+Sammie156@users.noreply.github.com>
2026-09-23 12:30:16 +0530

    Time: 0 ms (100%), Space: 10.4 MB (55.42%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     unordered_set<string> dict;
-    unordered_map<int, vector<string>> memo;
 
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         dict = unordered_set<string>(wordDict.begin(), wordDict.end());
@@ -14,18 +13,18 @@ public:
             return {""};
         }
 
-        if (memo.count(start)) return memo[start];
-
         vector<string> result;
 
         for (int end = start; end < s.size(); end++) {
             string word = s.substr(start, end - start + 1);
 
-            if (!dict.count(word)) continue;
+            if (!dict.count(word)) {
+                continue;
+            }
 
             vector<string> suffixes = dfs(s, end + 1);
 
-            for (string& suffix: suffixes) {
+            for (string suffix: suffixes) {
                 if (suffix.empty()) {
                     result.push_back(word);
                 } else {
@@ -33,9 +32,6 @@ public:
                 }
             }
         }
-
-        memo[start] = result;
-
 
         return result;
     }
